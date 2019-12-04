c82fb47449f236c3ad5eeb1c78b14d8cc50e691d
Ana Vassallo <aniavassallo@gmail.com>
2019-12-04 11:47:15 -0600

    Cambios en Móvil y detalles

--- Front/Static/js/scripts.js
+++ Front/Static/js/scripts.js
@@ -1,5 +1,4 @@
 const signUpButton = document.getElementById('signUp');
-// const registro = document.getElementById('registro');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
@@ -7,11 +6,21 @@ signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
-// registro.addEventListener('click', () => {
-// 	container.classList.add("right-panel-active");
-// });
-
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+document.getElementById("LOGIN").style.display = "block";
+document.getElementById("SIGNUP").style.display = "block";
+
+
+function cambiarASignUp() {
+	document.getElementById("LOGIN").style.display = "none";
+	document.getElementById("SIGNUP").style.display = "block";
+}
+
+function cambiarALogin() {
+	document.getElementById("SIGNUP").style.display = "none";
+	document.getElementById("LOGIN").style.display = "block";
+}
 
